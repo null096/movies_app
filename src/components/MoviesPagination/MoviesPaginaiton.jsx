ce4b8a752e6ad69207f26d5b55134378a828a208
nodejs <kanaradze223@gmail.com>
2018-10-10 17:11:25 +0300

    Pagination added

--- src/components/MoviesPagination/MoviesPaginaiton.jsx
+++ src/components/MoviesPagination/MoviesPaginaiton.jsx
@@ -3,34 +3,134 @@ import Proptypes from 'prop-types';
 import {
 	PAGE_WITH_MOVIES
 } from '../../constants/routes';
+import {
+	NUM_OF_NEAR_PAGES_WITH_NUMER,
+	NUM_OF_NEAR_PAGES_ON_ONE_SIDE,
+} from '../../constants/constants';
 import { Link } from 'react-router-dom';
 
 export class MoviesPaginaiton extends Component {
 	static propTypes = {
 		numOfPages: Proptypes.number.isRequired,
+		currentPage: Proptypes.number.isRequired,
 	};
 
-	render() {
+	isValidPage(page) {
 		const {
-			numOfPages
+			numOfPages,
 		} = this.props;
-		let pageLinks = [];
-		for (let i = 1; i <= numOfPages; i++) {
-			pageLinks.push(
-				<li key={i}>
-					<Link
-						to={`${PAGE_WITH_MOVIES}/${i}`}
-					>
-						{i}
+
+		return page > 0 && page <= numOfPages;
+	}
+
+	getFirstAndPrevLinks(currentPage) {
+		const isFirstAndPrevPagesValid = currentPage > 1;
+
+		return isFirstAndPrevPagesValid
+			? [
+				<Link to={`${PAGE_WITH_MOVIES}/${1}`}>
+					First
+				</Link>,
+				<Link to={`${PAGE_WITH_MOVIES}/${currentPage - 1}`}>
+					Prev
+				</Link>
+			]
+			: [
+				<span>First</span>,
+				<span>Prev</span>
+			];
+	}
+
+	getLastAndNextLinks(currentPage, numOfPages) {
+		const isLastAndNextPagesValid = currentPage !== numOfPages;
+
+		return isLastAndNextPagesValid
+			? [
+				<Link to={`${PAGE_WITH_MOVIES}/${currentPage + 1}`}>
+					Next
+				</Link>,
+				<Link to={`${PAGE_WITH_MOVIES}/${numOfPages}`}>
+					Last
+				</Link>
+			]
+			: [];
+	}
+
+	getNearPageWithNumber(currentPage, numOfPages) {
+		if (currentPage === 1) {
+			let res = [
+				<span>{currentPage}</span>,
+			];
+
+			for (let i = 1; i < NUM_OF_NEAR_PAGES_WITH_NUMER; i++) {
+				const pageNum = currentPage + i;
+				if (!this.isValidPage(pageNum)) break;
+				res.push(
+					<Link to={`${PAGE_WITH_MOVIES}/${pageNum}`}>
+						{currentPage + i}
 					</Link>
-				</li>
-			);
+				);
+			}
+			if (numOfPages > NUM_OF_NEAR_PAGES_WITH_NUMER) {
+				res.push(<span>...</span>);
+			}
+
+			return res;
 		}
+		if (currentPage > 0) {
+			let res = [];
+			for (let i = currentPage - NUM_OF_NEAR_PAGES_ON_ONE_SIDE;
+				i <= currentPage + NUM_OF_NEAR_PAGES_ON_ONE_SIDE;
+				i++
+			) {
+				if (!this.isValidPage(i)) continue;
+				if (i === currentPage) {
+					res.push(<span>{currentPage}</span>);
+				} else {
+					res.push(
+						<Link to={`${PAGE_WITH_MOVIES}/${i}`}>
+							{i}
+						</Link>
+					);
+				}
+			}
+			
+			return res;
+		}
+
+		return [];
+	}
+
+
+
+
+	getPagination() {
+		const {
+			numOfPages,
+			currentPage
+		} = this.props;
+		const pageLinks = [
+			...this.getFirstAndPrevLinks(currentPage),
+			...this.getNearPageWithNumber(currentPage, numOfPages),
+			...this.getLastAndNextLinks(currentPage, numOfPages),
+		];
+
+		return pageLinks;
+	}
+
+	render() {
+		const pageLinks = this.getPagination();
 
 		return (
 			<div className="pagination">
 				<ul>
-					{pageLinks}
+					{
+						pageLinks.map((item, i) => (
+							<li key={i}>
+								{item}
+							</li>
+						))
+					}
 				</ul>
 			</div>
 		);
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,5 @@
 export const URL_TO_MOVIES_NOW_PLAYING = 'http://api.themoviedb.org/3/movie/now_playing';
 export const MOVIE_IMAGE_SIZE = 185;
 export const URL_TO_MOVIE_IMAGE = 'http://image.tmdb.org/t/p/w' + MOVIE_IMAGE_SIZE;
+export const NUM_OF_NEAR_PAGES_WITH_NUMER = 3;
+export const NUM_OF_NEAR_PAGES_ON_ONE_SIDE = 1;
