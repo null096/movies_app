c158946f83053c80d6a28184289c70baf95c41bd
null096 <33376471+null096@users.noreply.github.com>
2018-10-27 15:37:29 +0300

    Var name fix

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
 export const URL_TO_MOVIES_NOW_PLAYING = 'http://api.themoviedb.org/3/movie/now_playing';
-export const ULR_TO_MOVIE_PHOTO = 'http://image.tmdb.org/t/p/';
-export const URL_TO_MOVIE_IMAGE_W154 = `${ULR_TO_MOVIE_PHOTO}w154`;
-export const URL_TO_MOVIE_IMAGE_W185 = `${ULR_TO_MOVIE_PHOTO}w185`;
-export const URL_TO_MOVIE_IMAGE_ORIGINAL = `${ULR_TO_MOVIE_PHOTO}original`;
+export const URL_TO_MOVIE_PHOTO = 'http://image.tmdb.org/t/p/';
+export const URL_TO_MOVIE_IMAGE_W154 = `${URL_TO_MOVIE_PHOTO}w154`;
+export const URL_TO_MOVIE_IMAGE_W185 = `${URL_TO_MOVIE_PHOTO}w185`;
+export const URL_TO_MOVIE_IMAGE_ORIGINAL = `${URL_TO_MOVIE_PHOTO}original`;
 export const MAX_NUM_OF_RTL_PAGES = 2; /* only even number */
 export const NUM_OF_RTL_PAGES = ~~(MAX_NUM_OF_RTL_PAGES / 2);
